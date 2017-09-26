c82985b56d6a3e621ee5d81b8bb75613b5dd5e9b
Patrick Lin <patricklin@alumni.ubc.ca>
2017-09-26 15:59:56 -0700

    rewrite file name with name / id

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,27 @@
 import { Router } from 'express';
-import { findApp, fillForm } from './database'
-import multer from 'multer'
-import path from 'path'
+import { findApp, fillForm } from './database';
+import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/')
   },
-  filename: (req, file, cb) =>{
-    cb(null, file.originalname)
+  filename: (req, file, cb) => {
+    console.log(req.body)
+    cb(null, `${req.body.firstName}-${req.body.lastName}-${req.body.id}` + '-' + file.originalname)
   }
 })
 
-const upload = multer({ 
+const upload = multer({
   storage,
   filterFilter: (req, file, cb) => {
-    if (file.minetype !== ('application/pdf' || 
+    if (file.minetype !== ('application/pdf' ||
       'application/mswordapplication/vnd.openxmlformats-officedocument.wordprocessingml.document' ||
       'image/jpeg')) {
-        return cb(null, false, new Error('Unsupported file format'))
-      }
+      return cb(null, false, new Error('Unsupported file format'))
+    }
   }
- });
+});
 
 const routes = Router();
 
@@ -39,7 +39,7 @@ routes.get('/api/login', (req, res) => {
 })
 
 routes.post('/api/form', upload.single('files'), (req, res) => {
-  
+
   // check headers for CWL attributes
   console.log(req.body)
 
