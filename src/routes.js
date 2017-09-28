609d8549afaaa951abce493ab096519fe3fe75e9
Patrick Lin <patricklin@alumni.ubc.ca>
2017-09-28 15:22:58 -0700

    fix file upload extension / variable changes

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,23 @@
 import { Router } from 'express';
-import { findApp, fillForm } from './database';
 import multer from 'multer';
-import path from 'path'
+import path from 'path';
+import { findApp, fillForm } from './database';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, './uploads/')
   },
   filename: (req, file, cb) => {
-    const timeStamp = Date.now();
-    cb(null, `${req.body.firstName}-${req.body.lastName}-${req.body.id}-${timeStamp}` + '.' + path.extname(file.originalname))
+    const timeStamp = new Date().toJSON().slice(0, 10).replace(/-/g, '-');
+    cb(null, `${req.body.id}-${req.body.firstName}-${req.body.lastName}-${timeStamp}${path.extname(file.originalname)}`)
   }
 })
 
 const upload = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
-    if (path.extname(file.originalname) != '.pdf' && path.extname(file.originalname) != '.docx' && path.extname(file.originalname) != '.doc' && path.extname != '.jpeg' && path.extname(file.originalname) != '.jpg') {
+    if (path.extname(file.originalname) !== '.pdf' && path.extname(file.originalname) !== '.docx' &&
+      path.extname(file.originalname) !== '.doc' && path.extname !== '.jpeg' && path.extname(file.originalname) !== '.jpg') {
       return cb(new Error('Unsupported file format'))
     }
     return cb(null, true)
@@ -25,13 +26,15 @@ const upload = multer({
 
 const routes = Router();
 
+const userUpload = upload.single('files')
+
 routes.get('/api/login', (req, res) => {
-  const profile = {
+  const user = {
     cwl: req.headers.cwlloginname, shibSN: req.headers.studentnumber,
     shibFirstName: req.headers.givenname, shibLastName: req.headers.sn
   };
-  //const profile = { cwl: 'unreg', id: 349274 }
-  findApp(profile, (err, result) => {
+  //const user = { cwl: 'unreg', id: 349274 }
+  findApp(user, (err, result) => {
     if (err) {
       console.log(err)
     } else {
@@ -40,17 +43,15 @@ routes.get('/api/login', (req, res) => {
   })
 })
 
-const userUpload = upload.single('files')
-
 routes.post('/api/form', (req, res) => {
-  const credentials = { cwl: req.headers.cwlloginname, id: req.headers.studentnumber };
+  const user = { cwl: req.headers.cwlloginname, id: req.headers.studentnumber };
   userUpload(req, res, (err) => {
     if (err) {
       console.log(err)
     }
   })
-  //const credentials = { cwl: 'unreg', id: 349274 };
-  fillForm(req.body, credentials, (err, result) => {
+  //const user = { cwl: 'unreg', id: 349274 };
+  fillForm(req.body, user, (err, result) => {
     if (err) {
       console.log(err)
     } else {
