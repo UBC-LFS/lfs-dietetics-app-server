a8151e48a070cf949d53c473be704d1aab490c7a
Justin Lee <justinlee0022@gmail.com>
2017-09-28 15:05:52 -0700

    console logging more

--- src/routes.js
+++ src/routes.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 const upload = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
-    console.log(path.extname(file.originalname) == '.pdf')
+    console.log(path.extname(file.originalname) != '.pdf' || path.extname(file.originalname) != '.docx' || path.extname(file.originalname) != '.doc' || path.extname != '.jpeg' || path.extname(file.originalname) != '.jpg')
     if (path.extname(file.originalname) != '.pdf' || path.extname(file.originalname) != '.docx' || path.extname(file.originalname) != '.doc' || path.extname != '.jpeg' || path.extname(file.originalname) != '.jpg') {
       return cb(new Error('Unsupported file format'))
     }
