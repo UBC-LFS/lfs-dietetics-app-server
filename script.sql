6579279251d31655112357f6036347f88da49d90
Patrick Lin <patricklin@alumni.ubc.ca>
2017-09-29 12:20:17 -0700

    add birthday to script

--- script.sql
+++ script.sql
@@ -11,6 +11,7 @@ CREATE TABLE Applicants
 	ID INT(10) NOT NULL,
 	Phone VARCHAR(250) NULL,
 	Email VARCHAR(250) NULL,
+	Birthday DATE NULL,
 	NumberOfApps VARCHAR(250) NULL,
 	Aboriginal VARCHAR(250) NULL,
 	AboriginalIdentity VARCHAR(250) NULL,
@@ -18,7 +19,7 @@ CREATE TABLE Applicants
 	CreateDate DATE NOT NULL,
 	PRIMARY KEY (CWL));
 
-INSERT INTO Applicants VALUES ('plin', 12345678, 'Patrick', 'Lin', 'Patrick', 'Lin', 12345678, 7783242213, 'patrick.lin@ubc.ca', 'yes', '', '', 186620, DATE '2017-09-25');
-INSERT INTO Applicants VALUES ('jLee', 48535678, 'Justin', 'Lee', 'Justin', 'Lee', 48535678, 7783242213, 'Justin.Lee@ubc.ca', 'no', 'no', '', 186521, DATE '2017-09-25');
-INSERT INTO Applicants VALUES ('jChan', 12343248, 'Johnathan', 'Chan', 'Johnathan', 'Chan', 12343248, 7783242213, 'johnathan.chan@ubc.ca', 'other', 'no', '', 186610, DATE '2017-09-25');
-INSERT INTO Applicants VALUES ('aLiu', 48564678, 'Aaron', 'Liu', 'Aaron', 'Liu', 48564678, 7783242213, 'Aaron.Liu@ubc.ca', 'yes', 'yes', 'first nation', 186521, DATE '2017-09-25');
+INSERT INTO Applicants VALUES ('plin', 12345678, 'Patrick', 'Lin', 'Patrick', 'Lin', 12345678, 7783242213, 'patrick.lin@ubc.ca', DATE '2017-09-25', 'yes', '', '', 186620, DATE '2017-09-25');
+INSERT INTO Applicants VALUES ('jLee', 48535678, 'Justin', 'Lee', 'Justin', 'Lee', 48535678, 7783242213, 'Justin.Lee@ubc.ca', DATE '2017-09-25', 'no', 'no', '', 186521, DATE '2017-09-25');
+INSERT INTO Applicants VALUES ('jChan', 12343248, 'Johnathan', 'Chan', 'Johnathan', 'Chan', 12343248, 7783242213, 'johnathan.chan@ubc.ca', DATE '2017-09-25', 'other', 'no', '', 186610, DATE '2017-09-25');
+INSERT INTO Applicants VALUES ('aLiu', 48564678, 'Aaron', 'Liu', 'Aaron', 'Liu', 48564678, 7783242213, 'Aaron.Liu@ubc.ca', DATE '2017-09-25', 'yes', 'yes', 'first nation', 186521, DATE '2017-09-25');
