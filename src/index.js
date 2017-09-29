21ffb5fcf789776659d10485097e53abf6c9df01
Justin Lee <justinlee0022@gmail.com>
2017-09-29 15:18:18 -0700

    Removed console message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import app from './app';
 
-console.log(process.env)
-
 const { PORT = 8080 } = process.env;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
