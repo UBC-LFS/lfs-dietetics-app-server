d1b97f5afc35741ff2ca07cd46a8d3f6d534965f
Justin Lee <justinlee0022@gmail.com>
2017-09-29 14:55:04 -0700

    Console out process.env

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import app from './app';
 
+console.log(process.env)
+
 const { PORT = 8080 } = process.env;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
