0ca79a381ffaad7eb3f5692caab97184547cc585
Justin Lee <justinlee0022@gmail.com>
2017-09-29 14:40:35 -0700

    shell script for starting production app

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-pm2 delete lfs-course-eval
-
 source .env
 
-npm install
-npm run test-server
+git pull
 npm run build
 
-pm2 -f start dist/lfs-course-eval.js
+pm2 -f start dist/app.js
