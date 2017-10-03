5df2f87d3d9d375f07e5bf9f3214f7d58c760ddf
Justin Lee <justinlee0022@gmail.com>
2017-10-03 08:41:46 -0700

    Added getconnection statement

--- src/database.js
+++ src/database.js
@@ -16,24 +16,33 @@ const findApp = (profile, callback) => {
     const cwl = profile.cwl;
     let shibSN = profile.shibSN;
     const sql = `SELECT * FROM ${table} WHERE CWL='${cwl}' AND ShibStudentNumber='${shibSN}';`;
-    c.query(sql, (error, results) => {
-        if (error || typeof results === 'undefined') {
-            callback(null, { type: 'error', filledForm: false, ApplicationNumber: '' });
-        } else {
-            results.length === 1 ? callback(null, { type: 'render', filledForm: true, ApplicationNumber: (results[0].ApplicationNumber) }) : callback(null, { type: 'render', filledForm: false, ApplicationNumber: '' });
-        }
-    });
+    c.getConnection((err, connection) => {
+        if (err) throw err
+        connection.query(sql, (error, results) => {
+            if (error || typeof results === 'undefined') {
+                callback(null, { type: 'error', filledForm: false, ApplicationNumber: '' });
+            } else {
+                results.length === 1 ? callback(null, { type: 'render', filledForm: true, ApplicationNumber: (results[0].ApplicationNumber) }) : callback(null, { type: 'render', filledForm: false, ApplicationNumber: '' });
+            }
+        })
+
+        connection.release()
+    })
 }
 
 const getPins = (callback) => {
     const query = `SELECT ApplicationNumber FROM ${table};`;
-    c.query(query, (err, res) => {
-        if (err) {
-            callback(err);
-            return;
-        } else {
-            callback(null, res);
-        }
+    c.getConnection((err, connection) => {
+        if (err) throw err
+        connection.query(query, (err, res) => {
+            if (err) {
+                callback(err);
+                return;
+            } else {
+                callback(null, res);
+            }
+        })
+        connection.release()
     })
 }
 
@@ -69,14 +78,16 @@ const fillForm = (form, file, profile, callback) => {
                                                         '${profile.shibLastName}', '${form.firstName}', '${form.lastName}',
                                                         '${form.id}', '${form.phone}', '${form.email}', '${form.birthday}', 
                                                         '${form.numOfApp}', '${form.aboriginal}', '${form.aborId}', '${pin}', '${pathArray[0]}', '${form.date}');`
-            c.query(query, function (error, rows) {
-                if (typeof rows.affectedRows === 'undefined') {
-                    callback(null, { type: 'error', filledForm: false, ApplicationNumber: '' });
-                }
-           
-                rows.affectedRows === 1 ? callback(null, { type: 'render', filledForm: true, ApplicationNumber: pin }) : callback(null, { type: 'error', filledForm: false, ApplicationNumber: '' });
-                
-            });
+            c.getConnection((err, connection) => {
+                if (err) throw err
+                connection.query(query, function (error, rows) {
+                    if (typeof rows.affectedRows === 'undefined') {
+                        callback(null, { type: 'error', filledForm: false, ApplicationNumber: '' });
+                    }
+                    rows.affectedRows === 1 ? callback(null, { type: 'render', filledForm: true, ApplicationNumber: pin }) : callback(null, { type: 'error', filledForm: false, ApplicationNumber: '' });
+                });
+                connection.release()
+            })
         }
     })
 }
